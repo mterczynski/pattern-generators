12a92d93bf9f63417e56218a9a79ba07f5efca20
Michał Terczyński <mterczynski1@gmail.com>
2023-11-14 00:41:04 +0100

    Add generate & download buttons

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,12 @@ export class App {
     this.canvas.width = innerWidth;
     this.canvas.height = innerHeight;
 
+    this.draw()
+    this.addGenerateButtonListener()
+    this.addDownloadButtonListener()
+  }
+
+  private draw() {
     this.drawBackground();
     this.patterns.line2.draw("rgba(3, 149, 240, 0.4)");
     this.patterns.line2.draw("rgba(0, 255, 128, 0.4)");
@@ -23,11 +29,33 @@ export class App {
     // this.patterns.line3.draw({red: 255, green: 255, blue: 255});
     // this.patterns.line3.draw({red: 0, green: 0, blue: 0});
     // this.patterns.line3.draw({red: 3, green: 149, blue: 240});
-
   }
 
   private drawBackground() {
     this.context.fillStyle = "rgb(0,0,0)";
     this.context.fillRect(0, 0, innerWidth, innerHeight);
+  }
+
+  private addGenerateButtonListener() {
+    const button = document.getElementById('button-generate')
+
+    button?.addEventListener('click', () => {
+      this.draw()
+    })
+  }
+
+  private addDownloadButtonListener() {
+    const button = document.getElementById('button-download')
+    const canvas = document.getElementById('main-canvas') as HTMLCanvasElement
+
+    button?.addEventListener('click', () => {
+      if (!canvas) {
+        return
+      }
+      const link = document.createElement('a');
+      link.download = 'blue-green-line-art.jpg';
+      link.href = canvas.toDataURL()
+      link.click();
+    })
   }
 }
