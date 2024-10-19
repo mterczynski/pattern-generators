77ead7b387f12ffb5e732af6abb9ee36673a5b96
Michał Terczyński <mterczynski1@gmail.com>
2024-10-20 00:33:12 +0200

    Add a script to deploy to Github Pages, remove Jenkinsfile and Travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
 # 1. Build
+npm run build;
 # 2. Copy build to ../mterczynski.github.io
+rm -rf ../mterczynski.github.io/pattern-generators/
+cp -r ./dist ../mterczynski.github.io/pattern-generators/
+cd ../mterczynski.github.io/pattern-generators/
 # 3. Commit in ../mterczynski.github.io
-# 4. Push in ../mterczynski.github.io
+git add .
+git commit -m "Update pattern-generators build: $(git log -1 --pretty=format:"%s")"
+git diff HEAD~1
+git status
+# 4. Do manually: push in ../mterczynski.github.io
+# git push
